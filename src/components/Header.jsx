d76b4cef2679367fdd3f292295dc02f525070574
richardgvini16-del <richard.gvini16@gmail.com>
2026-05-22 13:14:23 -0300

    quadra

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,34 @@
 import { FaBars } from 'react-icons/fa'
+import { useState } from 'react'
+
 import styles from '../styles/home.module.css'
 import logo from '../assets/logo.jpeg'
 
 function Header() {
+
+  const [menuAberto, setMenuAberto] = useState(false)
+
   return (
     <div className={styles.topHeader}>
 
-      <FaBars className={styles.menuIcon} />
+      <FaBars
+        className={styles.menuIcon}
+        onClick={() => setMenuAberto(!menuAberto)}
+      />
+
+      {menuAberto && (
+        <div className={styles.sidebarMenu}>
+
+          <img src={logo} alt="" />
+
+          <a href="">🏠 Início</a>
+          <a href="">⚽ Quadras</a>
+          <a href="">📅 Agendamentos</a>
+          <a href="">🏆 Torneios</a>
+          <a href="">💳 Pagamento</a>
+
+        </div>
+      )}
 
       <div className={styles.logoArea}>
 
@@ -22,7 +44,6 @@ function Header() {
       </div>
 
     </div>
-    
   )
 }
 
