ad8f2cd5830aaf2c6e8b1766231e8fddb761a4bf
richardgvini16-del <richard.gvini16@gmail.com>
2026-05-25 14:07:48 -0300

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,27 @@
+import { useState } from 'react'
 import styles from '../styles/home.module.css'
-
 import Header from '../components/quadras/Header.jsx'
 import CategoryButtons from '../components/quadras/CategoryButtons.jsx'
 import SearchBar from '../components/quadras/SearchBar.jsx'
 import QuadrasSection from '../components/quadras/QuadrasSection.jsx'
 import Pagination from '../components/quadras/Pagination.jsx'
-
 import { quadrasMckp } from '../mockup/dados.js';
-const [pagina, setPagina] = useState(1);
 
 function Home() {
+  const [pagina, setPagina] = useState(""); // ✅ CORRETO — dentro do componente
+
   return (
     <div className={styles.container}>
-
       <div className={styles.app}>
-
         <Header />
-
         <CategoryButtons />
-
         <SearchBar />
-
         <QuadrasSection quadras={quadrasMckp} />
-
-     <Pagination
-  pagina={pagina}
-  setPagina={setPagina}
-/>
-
+        <Pagination
+          pagina={pagina}
+          setPagina={setPagina}
+        />
       </div>
-
     </div>
   )
 }
