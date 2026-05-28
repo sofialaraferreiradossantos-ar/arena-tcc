da07f738f2b5bf4d246fe1e05193837e4d227fbd
sofialaraferreiradossantos-ar <sofialaraferreiradossantos@gmail.com>
2026-05-28 13:54:33 -0300

    menu meus agend
    meus agend

--- src/components/agendamento/HeaderAgendamentos.jsx
+++ src/components/agendamento/HeaderAgendamentos.jsx
@@ -1,26 +1,33 @@
+import { useState } from "react";
 import { FaBars, FaRegUserCircle } from "react-icons/fa";
-
 import styles from "../../styles/agendamentos.module.css";
-
 import logo from "../../assets/logo.jpeg";
 
 function HeaderAgendamentos() {
+  const [menuAberto, setMenuAberto] = useState(false);
+
   return (
-    <div className={styles.topHeader}>
-
-      <FaBars className={styles.menuIcon} />
-
-      <div className={styles.logoArea}>
-        <img src={logo} alt="logo" />
-
-        <h2>
-          Meus <span>Agendamentos</span>
-        </h2>
+    <>
+      <div className={styles.topHeader}>
+        <FaBars
+          className={styles.menuIcon}
+          onClick={() => setMenuAberto(!menuAberto)}
+        />
+        <div className={styles.logoArea}>
+          <img src={logo} alt="logo" />
+          <h2>Meus <span>Agendamentos</span></h2>
+        </div>
+        <FaRegUserCircle className={styles.userIcon} />
       </div>
 
-      <FaRegUserCircle className={styles.userIcon} />
-
-    </div>
+      {menuAberto && (
+        <div className={styles.sidebar}>
+          <a href="/">Home</a>
+          <a href="/agendamentos">Meus Agendamentos</a>
+          <a href="/quadra">Quadras</a>
+        </div>
+      )}
+    </>
   );
 }
 
