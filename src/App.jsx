70aa58bda0b59088cbea063bd86d9c804cc601c3
richardgvini16-del <richard.gvini16@gmail.com>
2026-06-10 14:41:09 -0300

    edicao da tela inicial
    menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
 import Home from "./pages/Home";
-import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <div className="app">
-
-      <Sidebar />
-
-      <div className="mainContent">
-        <Home />
-      </div>
-
+    <div>
+      <Home />
     </div>
   );
 }
